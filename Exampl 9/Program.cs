c8dd231303085ff8068d5219a1f902b738117b41
Сергей Кашмель <107143266+SiarheiKashmel@users.noreply.github.com>
2022-07-26 18:22:50 +0400

    Добавьте файлы проекта.

---
+++ Exampl 9/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[] nums = { 8, -45, 3, 25, 7 };
+Console.WriteLine($" {nums.Length} почти солнечных дней ) cтемпературой : 8, -45, 3, 25, 7 ");
+temperature(nums);
+
+static void temperature (int[] ints)
+{
+    int result = 0;
+    foreach (int number in ints)
+    {
+        if (number < 8)
+        {
+            result++;
+        }
+    }
+    Console.WriteLine($"Число дней ниже 8 градусов: {result}");
+}
